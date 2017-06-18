36ac805608314203a8222e04dd83aaa4a4280e63
Lanier <mdlanier@net.plm.eds.com>
2017-06-18 16:47:55 -0500

    scoreboard buttons

--- src/VScoreboard.vue
+++ src/VScoreboard.vue
@@ -1,5 +1,46 @@
 <template>
     <div style="position:static;">
+        <table>
+            <tr>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+                <td class="rectangle-box"></td>
+            </tr>
+            <tr>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+            </tr>
+            <tr>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+                <td class="square-box"></td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -10,4 +51,18 @@ export default {
 </script>
 
 <style>
+.square-box {
+    position: static;
+    height: 32px;
+    width: 32px;
+    border: 1px solid black;
+    border-radius: 5px;
+}
+.rectangle-box {
+    position: static;
+    height: 16px;
+    width: 32px;
+    border: 1px solid black;
+    border-radius: 5px;
+}
 </style>
