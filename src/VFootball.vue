7dc63218fa7db17b080f058eb5ce0d1121360467
Mike Lanier <Mike.Lanier1@comcast.net>
2017-06-18 13:23:13 -0500

    initial checkin

---
+++ src/VFootball.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" 
+        style="position:fixed;
+                left:5px;
+                top:5px; 
+                right:5px; 
+                bottom:5px; 
+                border:1px solid black; 
+                border-radius:5px; 
+                background-color:green">
+    <v-scoreboard height="20%" width="70%">
+    </v-scoreboard>
+  </div>
+</template>
+
+<script>
+import VScoreboard from "./VScoreboard.vue"
+export default {
+  name: 'v-football',
+  components: { VScoreboard },
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/VScoreboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div 
+        style="position:absolute;
+                left:10px;
+                top:10px;
+                border:1px solid black;
+                border-radius:5px;
+                background-color:white"
+    >
+        {{ width }}
+        {{ height }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "v-scoreboard",
+    props: [ 'width', 'height'],
+    mounted() {
+        console.log(this.width)
+        console.log(this.height)
+    }
+}
+</script>
+
+<style>
+</style>
