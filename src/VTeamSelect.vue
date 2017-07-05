40ec40c6dfd754662c5350780d87faff143b1054
Mike Lanier <Mike.Lanier1@comcast.net>
2017-07-04 20:28:30 -0500

    map array which, given the name of the selected team, returns the handle to the team data.

--- src/VTeamSelect.vue
+++ src/VTeamSelect.vue
@@ -120,7 +120,7 @@ import {South_Carolina_1984} from "./assets/Teams/CSV/South_Carolina_1984.js";
 // import {Tennessee_1951} from "./assets/Teams/CSV/Tennessee_1951.js";
 // import {Tennessee_1970} from "./assets/Teams/CSV/Tennessee_1970.js";
 // import {Tennessee_1996} from "./assets/Teams/CSV/Tennessee_1996.js";
-// import {Tennessee_1998} from "./assets/Teams/CSV/Tennessee_1998.js";
+import {Tennessee_1998} from "./assets/Teams/CSV/Tennessee_1998.js";
 // import {Texas_Christian_2000} from "./assets/Teams/CSV/Texas Christian_2000.js";
 // import {Texas_Tech_1973} from "./assets/Teams/CSV/Texas Tech_1973.js";
 // import {Texas1_1969} from "./assets/Teams/CSV/Texas1_1969.js";
@@ -150,8 +150,134 @@ export default {
     components: { VTeamSelectItem },
     data() {
         return {
-		   junk: SEC,
-		   team: Alabama_2012
+			junk: SEC,
+			team: Alabama_2012,
+			teams: [
+				// { key: "Air_Force_1985", value: Air_Force_1985 }, 
+				// { key: "Alabama_1978", value: Alabama_1978 }, 
+				// { key: "Alabama_1979", value: Alabama_1979 }, 
+				// { key: "Alabama_1992", value: Alabama_1992 }, 
+				// { key: "Alabama_2009", value: Alabama_2009 }, 
+				{ key: "Alabama_2012", value: Alabama_2012 }, 
+				// { key: "Arizona_State_1975", value: Arizona_State_1975 }, 
+				// { key: "Arizona_State_1996", value: Arizona_State_1996 }, 
+				// { key: "Arizona_1993", value: Arizona_1993 }, 
+				{ key: "Arkansas_1977", value: Arkansas_1977 }, 
+				// { key: "Army_1945", value: Army_1945 }, 
+				// { key: "Army_1996", value: Army_1996 }, 
+				// { key: "Army_2006", value: Army_2006 }, 
+				// { key: "Army_2009", value: Army_2009 }, 
+				// { key: "Auburn_1983", value: Auburn_1983 }, 
+				// { key: "Auburn_1993", value: Auburn_1993 }, 
+				// { key: "Auburn_2004", value: Auburn_2004 }, 
+				{ key: "Auburn_2010", value: Auburn_2010 }, 
+				// { key: "Baylor_1980", value: Baylor_1980 }, 
+				// { key: "Boise_State_2009", value: Boise_State_2009 }, 
+				// { key: "Boston_College_1984", value: Boston_College_1984 }, 
+				// { key: "Brigham_Young_1984", value: Brigham_Young_1984 }, 
+				// { key: "Clemson_1981", value: Clemson_1981 }, 
+				// { key: "Colorado_1989", value: Colorado_1989 }, 
+				// { key: "East_Carolina_1991", value: East_Carolina_1991 }, 
+				// { key: "Florida_State_1987", value: Florida_State_1987 }, 
+				// { key: "Florida_State_1999", value: Florida_State_1999 }, 
+				// { key: "Florida1_2008", value: Florida1_2008 }, 
+				// { key: "Florida_1996", value: Florida_1996 }, 
+				// { key: "Florida_2006", value: Florida_2006 }, 
+				{ key: "Florida_2008", value: Florida_2008 }, 
+				// { key: "Georgia_Tech_1990", value: Georgia_Tech_1990 }, 
+				{ key: "Georgia_1980", value: Georgia_1980 }, 
+				// { key: "Houston_1979", value: Houston_1979 }, 
+				// { key: "Houston_1989", value: Houston_1989 }, 
+				// { key: "Indiana_1967", value: Indiana_1967 }, 
+				// { key: "Iowa_1985", value: Iowa_1985 }, 
+				// { key: "Kansas_1968", value: Kansas_1968 }, 
+				{ key: "Kentucky_1977", value: Kentucky_1977 }, 
+				// { key: "LSU_1987", value: LSU_1987 }, 
+				// { key: "LSU_2003", value: LSU_2003 }, 
+				{ key: "LSU_2007", value: LSU_2007 }, 
+				// { key: "Maryland_1951", value: Maryland_1951 }, 
+				// { key: "Maryland_1976", value: Maryland_1976 }, 
+				// { key: "Miami_1987", value: Miami_1987 }, 
+				// { key: "Miami_1991", value: Miami_1991 }, 
+				// { key: "Miami_2001", value: Miami_2001 }, 
+				// { key: "Miami_2002", value: Miami_2002 }, 
+				// { key: "Michigan_State_1966", value: Michigan_State_1966 }, 
+				// { key: "Michigan_1947", value: Michigan_1947 }, 
+				// { key: "Michigan_1969", value: Michigan_1969 }, 
+				// { key: "Michigan_1973", value: Michigan_1973 }, 
+				// { key: "Michigan_1997", value: Michigan_1997 }, 
+				// { key: "Minnesota_2003", value: Minnesota_2003 }, 
+				{ key: "Mississippi_2003", value: Mississippi_2003 }, 
+				{ key: "Missouri_1969", value: Missouri_1969 }, 
+				// { key: "Navy_1963", value: Navy_1963 }, 
+				// { key: "Navy_2004", value: Navy_2004 }, 
+				// { key: "Navy_2006", value: Navy_2006 }, 
+				// { key: "Navy_2009", value: Navy_2009 }, 
+				// { key: "Nebraska_1971", value: Nebraska_1971 }, 
+				// { key: "Nebraska_1983", value: Nebraska_1983 }, 
+				// { key: "Nebraska_1995", value: Nebraska_1995 }, 
+				// { key: "North_Carolina_1980", value: North_Carolina_1980 }, 
+				// { key: "North_Carolina_1997", value: North_Carolina_1997 }, 
+				// { key: "Northwestern_1995", value: Northwestern_1995 }, 
+				// { key: "Notre_Dame1_1966", value: Notre_Dame1_1966 }, 
+				// { key: "Notre_Dame_1966", value: Notre_Dame_1966 }, 
+				// { key: "Notre_Dame_1970", value: Notre_Dame_1970 }, 
+				// { key: "Notre_Dame_1973", value: Notre_Dame_1973 }, 
+				// { key: "Notre_Dame_1977", value: Notre_Dame_1977 }, 
+				// { key: "Notre_Dame_1988", value: Notre_Dame_1988 }, 
+				// { key: "Notre_Dame_2012", value: Notre_Dame_2012 }, 
+				// { key: "Ohio_State1_2002", value: Ohio_State1_2002 }, 
+				// { key: "Ohio_State_1973", value: Ohio_State_1973 }, 
+				// { key: "Ohio_State_1996", value: Ohio_State_1996 }, 
+				// { key: "Ohio_State_2002", value: Ohio_State_2002 }, 
+				// { key: "Ohio_State_2006", value: Ohio_State_2006 }, 
+				// { key: "Ohio_State_2007", value: Ohio_State_2007 }, 
+				// { key: "Oklahoma_State_1988", value: Oklahoma_State_1988 }, 
+				// { key: "Oklahoma_1974", value: Oklahoma_1974 }, 
+				// { key: "Oklahoma_1978", value: Oklahoma_1978 }, 
+				// { key: "Oklahoma_1986", value: Oklahoma_1986 }, 
+				// { key: "Oklahoma_2000", value: Oklahoma_2000 }, 
+				// { key: "Oklahoma_2004", value: Oklahoma_2004 }, 
+				// { key: "Oklahoma_2008", value: Oklahoma_2008 }, 
+				// { key: "Oregon_2001", value: Oregon_2001 }, 
+				// { key: "Oregon_2010", value: Oregon_2010 }, 
+				// { key: "Penn_State_1969", value: Penn_State_1969 }, 
+				// { key: "Penn_State_1973", value: Penn_State_1973 }, 
+				// { key: "Penn_State_1986", value: Penn_State_1986 }, 
+				// { key: "Penn_State_1994", value: Penn_State_1994 }, 
+				// { key: "Pittsburgh_1976", value: Pittsburgh_1976 }, 
+				// { key: "Pittsburgh_1981", value: Pittsburgh_1981 }, 
+				// { key: "San_Diego_State_1969", value: San_Diego_State_1969 }, 
+				// { key: "SMU_1982", value: SMU_1982 }, 
+				{ key: "South_Carolina_1984", value: South_Carolina_1984 }, 
+				// { key: "Syracuse_1987", value: Syracuse_1987 }, 
+				// { key: "Tennessee_1951", value: Tennessee_1951 }, 
+				// { key: "Tennessee_1970", value: Tennessee_1970 }, 
+				// { key: "Tennessee_1996", value: Tennessee_1996 }, 
+				{ key: "Tennessee_1998", value: Tennessee_1998 }, 
+				// { key: "Texas_Christian_2000", value: Texas_Christian_2000 }, 
+				// { key: "Texas_Tech_1973", value: Texas_Tech_1973 }, 
+				// { key: "Texas1_1969", value: Texas1_1969 }, 
+				// { key: "Texas_1969", value: Texas_1969 }, 
+				// { key: "Texas_1977", value: Texas_1977 }, 
+				// { key: "Texas_2005", value: Texas_2005 }, 
+				// { key: "Texas_2009", value: Texas_2009 }, 
+				// { key: "UCLA_1988", value: UCLA_1988 }, 
+				// { key: "UCLA_1998", value: UCLA_1998 }, 
+				// { key: "USC1_2004", value: USC1_2004 }, 
+				// { key: "USC_1967", value: USC_1967 }, 
+				// { key: "USC_1972", value: USC_1972 }, 
+				// { key: "USC_1979", value: USC_1979 }, 
+				// { key: "USC_2004", value: USC_2004 }, 
+				// { key: "USC_2005", value: USC_2005 }, 
+				// { key: "Utah_2008", value: Utah_2008 }, 
+				// { key: "Virginia_Tech_1999", value: Virginia_Tech_1999 }, 
+				// { key: "Washington_1984", value: Washington_1984 }, 
+				// { key: "Washington_1991", value: Washington_1991 }, 
+				// { key: "West_Virginia_1988", value: West_Virginia_1988 }, 
+				// { key: "Wisconsin_1993", value: Wisconsin_1993 }, 
+				// { key: "Wyoming_1967", value: Wyoming_1967 }			   
+			]
         }
     },
     watch: {
@@ -168,6 +294,16 @@ export default {
 			this.closeNav();
 			console.log("loadteam: " + team);
 			console.log("this.team.name: " + this.team.name);
+
+			var n = this.teams.length;
+			for( var i=0; i<n; i++ )
+			{
+				if(team == this.teams[i].key) {
+					this.team = this.teams[i].value;
+					break;
+				}
+			}
+
 			var team = new JTeam(this.team);
 		}
     },
